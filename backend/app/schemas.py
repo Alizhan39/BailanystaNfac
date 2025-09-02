f7017e534f4423fa521c4dfa4e64095c279a4223
Alizhan <92807091+Alizhan39@users.noreply.github.com>
2025-09-02 10:19:42 +0100

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -4,17 +4,17 @@ from datetime import datetime
 
 
 class UserOut(BaseModel):
-id: int
-username: str
-created_at: datetime
+    id: int
+    username: str
+    created_at: datetime
 
 
 class PostIn(BaseModel):
-text: str = Field(min_length=1, max_length=1000)
+    text: str = Field(min_length=1, max_length=1000)
 
 
 class PostOut(BaseModel):
-id: int
-text: str
-created_at: datetime
-author: UserOut
+    id: int
+    text: str
+    created_at: datetime
+    author: UserOut
