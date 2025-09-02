d4425cb723bd01b9a969b9b9a260fc0802bceccc
Alizhan <92807091+Alizhan39@users.noreply.github.com>
2025-09-02 10:48:44 +0100

    Update auth.ts

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -5,22 +5,22 @@ type State = { token: string | null; username: string | null }
 
 
 type Actions = {
-setAuth: (token: string, username: string) => void
-clear: () => void
+  setAuth: (token: string, username: string) => void
+  clear: () => void
 }
 
 
 export const useAuth = create<State & Actions>((set) => ({
-token: localStorage.getItem('token'),
-username: localStorage.getItem('username'),
-setAuth: (token, username) => {
-localStorage.setItem('token', token)
-localStorage.setItem('username', username)
-set({ token, username })
-},
-clear: () => {
-localStorage.removeItem('token')
-localStorage.removeItem('username')
-set({ token: null, username: null })
-},
+  token: localStorage.getItem('token'),
+  username: localStorage.getItem('username'),
+  setAuth: (token, username) => {
+    localStorage.setItem('token', token)
+    localStorage.setItem('username', username)
+    set({ token, username })
+  },
+  clear: () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('username')
+    set({ token: null, username: null })
+  },
 }))
