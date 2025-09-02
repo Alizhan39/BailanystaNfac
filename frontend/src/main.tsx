4c8c786ac4a436337fc757463bee56c35e813493
Alizhan <92807091+Alizhan39@users.noreply.github.com>
2025-09-02 10:49:55 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,19 +8,19 @@ import ProfilePage from './pages/ProfilePage'
 
 
 const router = createBrowserRouter([
-{
-path: '/',
-element: <App />,
-children: [
-{ index: true, element: <FeedPage /> },
-{ path: 'profile', element: <ProfilePage /> },
-],
-},
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <FeedPage /> },
+      { path: 'profile', element: <ProfilePage /> },
+    ],
+  },
 ])
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-<React.StrictMode>
-<RouterProvider router={router} />
-</React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
